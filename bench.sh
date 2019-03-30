096a1770fa5e5ec3ac914e87b34df9d8e535b859
freddy33 <freds@jfrog.com>
2019-03-30 18:53:09 +0300

    Starting env for pint benchmarking

--- bench.sh
+++ bench.sh
@@ -1,5 +1,25 @@
 #!/usr/bin/env bash
 
-go test -parallel 4 -cpuprofile cpu.prof -memprofile mem.prof -run='^$' -bench=. ./m3space/ >> ./docs/BenchResults.txt
-go tool pprof --text cpu.prof > ./docs/BenchPprofCpuResults.txt
-go tool pprof --text mem.prof > ./docs/BenchPprofMemResults.txt
+mkdir -p perf-data
+
+pack="$1"
+if [[ -z "$pack" ]]; then
+    echo "Usage $0 [package name = point, space, gl]"
+    exit 1
+fi
+
+runAndSave() {
+    local packageName="$1"
+    go test -parallel 4 -cpuprofile perf-data/cpu-${packageName}.prof -memprofile perf-data/mem-${packageName}.prof -run='^$' -bench=. ./m3${packageName}/ >> ./docs/${packageName}-BenchResults.txt
+    go tool pprof --text perf-data/cpu-${packageName}.prof > ./docs/${packageName}-BenchPprofCpuResults.txt
+    go tool pprof --text perf-data/mem-${packageName}.prof > ./docs/${packageName}-BenchPprofMemResults.txt
+}
+
+if [[ "$pack" == "point" ] || [ "$pack" == "space" ] || [ "$pack" == "gl" ]]; then
+    runAndSave ${pack}
+    exit $?
+fi
+
+echo "Usage $0 [package name = point, space, gl]"
+echo "Package $pack unknown"
+exit 2
