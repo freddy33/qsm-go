4e7dfc558a9330ca098ff16d54927c40aa6ad3d7
freddy33 <freds@jfrog.com>
2019-06-28 10:32:18 +0300

    Started small db framework

--- m3db/m3db.go
+++ m3db/m3db.go
@@ -11,6 +11,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"sync"
 )
 
 var Log = m3util.NewLogger("m3db", m3util.INFO)
@@ -32,6 +33,38 @@ type DbConnDetails struct {
 	User     string `json:"user"`
 	Password string `json:"password"`
 	DbName   string `json:"dbName"`
+}
+
+type QsmError string
+
+func (qsmError QsmError) Error() string {
+	return string(qsmError)
+}
+
+type TableDefinition struct {
+	Name       string
+	Checked    bool
+	Created    bool
+	DdlColumns string
+	InsertStmt string
+	InsertFunc func(stmt *sql.Stmt) (sql.Result, error)
+}
+
+type TableExec struct {
+	TableDef   *TableDefinition
+	Db         *sql.DB
+	InsertStmt *sql.Stmt
+}
+
+var createTableMutex sync.Mutex
+var tableDefinitions map[string]*TableDefinition
+
+func init() {
+	tableDefinitions = make(map[string]*TableDefinition)
+}
+
+func AddTableDef(tDef *TableDefinition) {
+	tableDefinitions[tDef.Name] = tDef
 }
 
 func readDbConf(envNumber QsmEnvironment) DbConnDetails {
@@ -78,5 +111,91 @@ func GetConnection(envNumber QsmEnvironment) *sql.DB {
 
 func CloseDb(db *sql.DB) {
 	m3util.ExitOnError(db.Close())
+}
+
+func CloseTableExec(te *TableExec) {
+	if te == nil || te.Db == nil {
+		return
+	}
+	err := te.Db.Close()
+	if err != nil {
+		Log.Error(err)
+	}
+}
+
+func GetTableExec(envNumber QsmEnvironment, tableName string) (*TableExec, error) {
+	tableExec := TableExec{}
+	tableExec.Db = GetConnection(envNumber)
+	err := tableExec.getOrCreateTable(tableName)
+	if err != nil {
+		Log.Error(err)
+		return nil, err
+	}
+	err = tableExec.fillStmt()
+	if err != nil {
+		Log.Error(err)
+		return nil, err
+	}
+	return &tableExec, nil
+}
+
+
+func (te *TableExec) fillStmt() error {
+	stmt, err := te.Db.Prepare(fmt.Sprintf("insert into %s "+te.TableDef.InsertStmt, te.TableDef.Name))
+	if err != nil {
+		Log.Error(err)
+		return err
+	}
+	te.InsertStmt = stmt
+	return nil
+}
+
+func (te *TableExec) getOrCreateTable(tableName string) error {
+	createTableMutex.Lock()
+	defer createTableMutex.Unlock()
+
+	var ok bool
+	te.TableDef, ok = tableDefinitions[tableName]
+	if !ok {
+		return QsmError(fmt.Sprintf("Table definition for %s does not exists", tableName))
+	}
+	if te.TableDef.Checked {
+		if Log.IsTrace() {
+			Log.Tracef("Table %s already checked", tableName)
+		}
+		return nil
+	}
+	checkQuery := fmt.Sprintf("select 1 from information_schema.tables where table_schema='public' and table_name='%s'", tableName)
+	resCheck, err := te.Db.Query(checkQuery)
+	if err != nil {
+		Log.Errorf("could not check if table %s exists using '%s' due to error %v", tableName, checkQuery, err)
+		return err
+	}
+	toCreate := !resCheck.Next()
+
+	if !toCreate {
+		if Log.IsDebug() {
+			Log.Debugf("Table %s already exists", tableName)
+		}
+		te.TableDef.Created = false
+		te.TableDef.Checked = true
+		return nil
+	}
+
+	if Log.IsDebug() {
+		Log.Debugf("Creating table %s", tableName)
+	}
+	createQuery := fmt.Sprintf("create table if not exists %s "+te.TableDef.DdlColumns, tableName)
+	_, err = te.Db.Exec(createQuery)
+	if err != nil {
+		Log.Errorf("could not create table %s using '%s' due to error %v", tableName, createQuery, err)
+		return err
+	}
+	if Log.IsDebug() {
+		Log.Debugf("Table %s created", tableName)
+	}
+	te.TableDef.Created = true
+	te.TableDef.Checked = true
+	return nil
 }
 
--- m3point/m3pointdb.go
+++ m3point/m3pointdb.go
@@ -1,80 +1,62 @@
 package m3point
 
 import (
-	"database/sql"
 	"fmt"
 	"github.com/freddy33/qsm-go/m3db"
-	"sync"
 )
 
-type QsmError string
 
-func (qsmError QsmError) Error() string {
-	return string(qsmError)
-}
-
-var createTableMutex sync.Mutex
-var createdTable map[string]bool
+const (
+	CONNECTION_DETAILS_TABLE = "connection_details"
+)
 
 func init() {
-	createdTable = make(map[string]bool)
+	m3db.AddTableDef(createConnectionDetailsTableDef())
+}
+
+func createConnectionDetailsTableDef() *m3db.TableDefinition {
+	res := m3db.TableDefinition{}
+	res.Name = CONNECTION_DETAILS_TABLE
+	res.Created = false
+	res.DdlColumns = fmt.Sprintf("(id smallint, x integer, y integer, z integer, ds bigint, constraint %s_pkey primary key (id))", res.Name)
+	res.InsertStmt = "(id,x,y,z,ds) values ($1,$2,$3,$4,$5)"
+	return &res
 }
 
 func saveAllConnectionDetails(envNumber m3db.QsmEnvironment) (int, error) {
-	db := m3db.GetConnection(envNumber)
-	defer m3db.CloseDb(db)
-
-	tableName, err := checkConnDetailsTable(db)
+	te, err := m3db.GetTableExec(envNumber, CONNECTION_DETAILS_TABLE)
+	defer m3db.CloseTableExec(te)
 	if err != nil {
-		return 0, err
-	}
-
-	stmt, err := db.Prepare(fmt.Sprintf("insert into %s(id,x,y,z,ds) values($1,$2,$3,$4,$5)", tableName))
-	if err != nil {
-		Log.Error(err)
 		return 0, err
 	}
 
 	inserted := 0
-	for _, cd := range allConnections {
-		res, err := stmt.Exec(cd.Id, cd.Vector.X(), cd.Vector.Y(), cd.Vector.Z(), cd.ConnDS)
-		if err != nil {
-			Log.Error(err)
-			return inserted, err
+	if te.TableDef.Created {
+		for _, cd := range allConnections {
+			res, err := te.InsertStmt.Exec(cd.Id, cd.Vector.X(), cd.Vector.Y(), cd.Vector.Z(), cd.ConnDS)
+
+			if err != nil {
+				Log.Error(err)
+				return inserted, err
+			}
+			rows, err := res.RowsAffected()
+			if err != nil {
+				Log.Error(err)
+				return inserted, err
+			}
+			if Log.IsTrace() {
+				Log.Tracef("inserted %v got %d response", *cd, rows)
+			}
+			if rows != int64(1) {
+				err = m3db.QsmError(fmt.Sprintf("should have receive one result, and got %d", rows))
+				Log.Error(err)
+				return inserted, err
+			}
+			inserted++
 		}
-		rows, err := res.RowsAffected()
-		if err != nil {
-			Log.Error(err)
-			return inserted, err
-		}
-		if Log.IsTrace() {
-			Log.Tracef("inserted %v got %d response", *cd, rows)
-		}
-		if rows != int64(1) {
-			err = QsmError(fmt.Sprintf("should have receive one result, and got %d", rows))
-			Log.Error(err)
-			return inserted, err
-		}
-		inserted++
+	} else {
+		Log.Debugf("Connection details table already created")
 	}
 	return inserted, nil
 }
 
-func checkConnDetailsTable(db *sql.DB) (string, error) {
-	createTableMutex.Lock()
-	defer createTableMutex.Unlock()
-
-	tableName := "connection_details"
-	done, ok := createdTable[tableName]
-	if ok && done {
-		return tableName, nil
-	}
-	r, err := db.Exec(fmt.Sprintf("create table if not exists %s (id smallint, x integer, y integer, z integer, ds bigint, constraint %s_pkey primary key (id))", tableName, tableName))
-	if err != nil {
-		Log.Error(err)
-		return "", err
-	}
-	fmt.Println(r)
-	createdTable[tableName] = true
-	return tableName, nil
-}
