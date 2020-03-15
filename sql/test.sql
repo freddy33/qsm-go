abc953fa56710f4f309617a6e2299c943d3b24e4
NivOvadia <niv.ovadia25@gmail.com>
2020-03-15 18:07:32 +0200

    Update test.sql

--- sql/test.sql
+++ sql/test.sql
@@ -17,7 +17,7 @@ order by d;
 select pn.d, p.x, p.y, p.z, sqrt(p.x*p.x+p.y*p.y+p.z*p.z) cart_d
 from path_nodes as pn
     join points as p on pn.point_id = p.id
-where pn.path_ctx_id=35 and pn.d=8
+where pn.path_ctx_id=35 and pn.d=53
 order by cart_d;
 
 
