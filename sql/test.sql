3511bca5a697bf3b7c7c3f0b42b8dc688fb6361b
NivOvadia <niv.ovadia25@gmail.com>
2020-04-01 13:29:06 +0300

    select cart_d

--- sql/test.sql
+++ sql/test.sql
@@ -16,7 +16,7 @@ group by path_ctx_id;
 
 select d, count(*)
 from path_nodes
-where path_ctx_id = 35
+where path_ctx_id = 37
 group by d
 order by d;
 
@@ -24,7 +24,7 @@ order by d;
 select pn.d, p.x, p.y, p.z, sqrt(p.x*p.x+p.y*p.y+p.z*p.z) cart_d
 from path_nodes as pn
     join points as p on pn.point_id = p.id
-where pn.path_ctx_id=35 and pn.d<=6
+where pn.path_ctx_id=37 and pn.d<=120
 order by cart_d;
 
 
@@ -32,7 +32,7 @@ select ct.d, count(ct.id), avg(ct.cart_d),avg(ct.cart_d)/ct.d "ratio(avg/d)", mi
 from (select pn.d d, pn.id id, sqrt(p.x * p.x + p.y * p.y + p.z * p.z) cart_d
       from path_nodes as pn
                join points as p on pn.point_id = p.id
-      where pn.path_ctx_id = 35 and pn.d=49) ct
+      where pn.path_ctx_id = 37 and pn.d=120) ct
 group by d;
 
 
@@ -41,7 +41,7 @@ select ct.d, count(ct.id), min(ct.cart_d), max(ct.cart_d),
 from (select pn.d d, pn.id id, sqrt(p.x * p.x + p.y * p.y + p.z * p.z) cart_d
       from path_nodes as pn
                join points as p on pn.point_id = p.id
-      where pn.path_ctx_id = 35 and pn.d>=1 and pn.d<=53) ct
+      where pn.path_ctx_id = 37 and pn.d>=1 and pn.d<=120) ct
 group by d
 order by d;
 
